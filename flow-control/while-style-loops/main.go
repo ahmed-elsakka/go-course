027f7db7376d2ef0a00883860b577cd995c53af9
ahmed-elsakka <ahmed.elsakka@outlook.com>
2025-09-16 22:43:26 +0100

    Add flow control projects

--- flow-control/while-style-loops/main.go
+++ flow-control/while-style-loops/main.go
@@ -5,15 +5,20 @@ import "fmt"
 func main() {
 	var enteredPin int
 	const userPin = 1234
+	attempt := 1
 
-	for attempt := 1; attempt <= 3; attempt++ {
+	// while a condition is true, keep executing
+	for attempt <= 3 {
 		fmt.Println("Enter the PIN: ")
 		fmt.Scanln(&enteredPin)
 
 		if enteredPin == userPin {
+			fmt.Println("Logged in")
 			break
 		} else {
 			fmt.Println("Incorrect PIN")
 		}
+
+		attempt++
 	}
 }
